3a381d8f2a4dd424566bdd5662e2821a85a55d74
Mphuong <email@cuaban.com>
2026-01-08 20:12:14 +0700

    m

--- NguyenVanQuyet-2210900120-DATN/admin/index.php
+++
@@ -1,7 +0,0 @@
-
-<?php 
-include __DIR__ . '/../includes/config.php';
-include __DIR__ . '/../views/admin/layout/header.php';
-include __DIR__ . '/../views/admin/master.php';
-include __DIR__ . '/../views/admin/layout/footer.php';
-?>
--- NguyenVanQuyet-2210900120-DATN/index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include('includes/config.php');
-include('pages/layout/header.php');
-include('pages/layout/carousel_start.php');
-include('pages/master.php');
-include('pages/layout/footer.php');
-?>
